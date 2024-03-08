cb5d293513f5e2e90ebbfe3750e54e33408ea8cb
nxkzty <leon.hoefferer@gmail.com>
2024-03-08 14:31:11 +0100

    halo jeremy

--- pages/roadmap.js
+++ pages/roadmap.js
@@ -1,19 +1,34 @@
 import React from 'react'; 
 import { Timeline } from 'primereact/timeline';
+import { Card } from 'primereact/card';
+import { Button } from 'primereact/button';
 
 export default function App() {
     const events = [
-        { status: 'Gegründet', date: '15/7/2024', icon: 'pi pi-shopping-cart', color: '#9C27B0', image: 'game-controller.jpg' },
-        { status: '1. Mal Smallgroup', date: '15/10/2024', icon: 'pi pi-cog', color: '#673AB7' },
-        { status: 'Erstellung von Notion', date: '15/10/2024', icon: 'pi pi-shopping-cart', color: '#FF9800' },
-        { status: 'Website', date: '16/10/2024', icon: 'pi pi-check', color: '#607D8B' }
+        { status: 'Gegründet', date: '15/7/2024', icon: 'pi pi-shopping-cart', color: 'lightGray', image: 'game-controller.jpg' },
+        { status: '1. Mal Smallgroup', date: '15/10/2024', icon: 'pi pi-cog', color: 'lightGray' },
+        { status: 'Erstellung von Notion', date: '15/10/2024', icon: 'pi pi-shopping-cart', color: 'lightGray'},
+        { status: 'Website', date: '16/10/2024', icon: 'pi pi-check', color: 'lightGray' }
     ];
+    const customizedMarker = (item) => {
+        return (
+            <span className="flex w-2rem h-2rem align-items-center justify-content-center text-white border-circle z-1 shadow-1" style={{ backgroundColor: item.color, padding: "5px", height: "25px", width: "25px", borderRadius: "5px"}}>
+                <i className={item.icon}></i>
+            </span>
+        );
+    };
+
+    const customizedContent = (item) => {
+        return (
+            <Card title={item.status} subTitle={item.date}>
+                <p>{item.status}</p>
+            </Card>
+        );
+    };
         
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <div className="card">
-                <Timeline value={events} opposite={(item) => item.status} content={(item) => <small className="text-color-secondary">{item.date}</small>} />
-            </div>
+        <div className="card">
+            <Timeline value={events} align="alternate" className="customized-timeline" marker={customizedMarker} content={customizedContent} style={{margin: "20px"}} />
         </div>
-    );
+    )
 }
