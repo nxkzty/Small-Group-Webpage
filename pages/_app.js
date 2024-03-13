3dc0fff81afa9dc43041967a3073571e3a178d5d
sheftj <sheftj@bbcag.ch>
2024-03-13 08:23:12 +0100

    [UPDATE]Souwwyyleudee

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Navbar from '../components/Navbar.js'
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import { useSession } from "@/lib/hooks/session"
 import { useAuthRedirect } from "@/lib/hooks/authredirect"
+import Head from 'next/head.js';
 
 
 export default function MyApp({ Component, pageProps }) {
@@ -12,6 +13,9 @@ export default function MyApp({ Component, pageProps }) {
     return (
 
         <>
+            <Head>
+                <link rel="icon" href="https://cdn1.iconfinder.com/data/icons/winter-119/48/Jesus_Christ-512.png" type="image/x-icon"></link>
+            </Head>
             <Navbar/>
 
 
